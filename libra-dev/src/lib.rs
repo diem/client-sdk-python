c84476af275a6d4ea50d382ceef6e62d598fa928
SunMi Lee <sunmilee0330@gmail.com>
2019-11-12 11:20:55 -0800

    [libra-dev] Adding account resource API (#3)

--- libra-dev/src/lib.rs
+++ libra-dev/src/lib.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod account_resource;
+mod data;

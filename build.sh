6122d2d7d22a069e9a06689f9813cd57bc2e0add
SunMi Lee <sunmilee@calibra.com>
2019-11-13 17:10:17 -0800

    [libra-dev] serialize signed transaction

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -xv
 set -euo pipefail
 
+# Create C to Rust bindings
+#bindgen libra-dev/include/data.h --whitelist-type=CEventHandle --whitelist-type=CDevAccountResource -o libra-dev/src/data.rs
+
 # Build libra-dev first
 cd libra-dev
 cargo build
--- libra-dev/src/lib.rs
+++ libra-dev/src/lib.rs
@@ -7,4 +7,4 @@
 mod data;
 
 pub mod account_resource;
-
+pub mod transaction;

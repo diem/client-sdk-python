a5bc6575303374b0b5e129528fbc0d123f565f14
Yucong Sun <fallentree@fb.com>
2020-01-27 08:22:28 -0800

    Update to new testnet release (#67)

--- python/test.sh
+++ python/test.sh
@@ -7,10 +7,10 @@ set -euo pipefail
 # seems to be circleCI specific
 ./venv/bin/pip3 install --upgrade pytest
 ./venv/bin/pip3 install --upgrade black
-#./venv/bin/pip3 install --upgrade pyre-check
+./venv/bin/pip3 install --upgrade pyre-check
 
 ./venv/bin/python3 setup.py develop
 
 ./venv/bin/python3 setup.py pytest --addopts="--runxfail --pylama . $@"
 ./venv/bin/black --check .
-#./venv/bin/pyre check
+./venv/bin/pyre check

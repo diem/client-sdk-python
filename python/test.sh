dd5f1b1f758941625663d575fc8d9164848cd3d6
Yucong Sun <fallentree@fb.com>
2020-02-24 16:35:48 -0800

    Lock to pyre-check 0.0.41 for now

--- python/test.sh
+++ python/test.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # seems to be circleCI specific
 ./venv/bin/pip3 install --upgrade pytest
 ./venv/bin/pip3 install --upgrade black
-./venv/bin/pip3 install --upgrade pyre-check
+./venv/bin/pip3 install --upgrade pyre-check==0.0.41
 
 ./venv/bin/python3 setup.py develop
 

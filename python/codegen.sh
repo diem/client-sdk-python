8d5a7de139f7168288978c0b960731f4966fd21b
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-22 18:10:35 -0700

    [codegen] remove call to sed

--- python/codegen.sh
+++ python/codegen.sh
@@ -2,17 +2,15 @@
 set -euo pipefail
 
 # Generate python code for Libra types and LCS runtime
-cargo install serde-generate --version 0.2.1
+cargo install serde-generate --version 0.2.2
 
 $HOME/.cargo/bin/serdegen\
     --language python3\
     --name libra_types\
     --with-runtimes serde lcs\
+    --package-name ..\
     --target-source-dir src/pylibra\
     ../libra/testsuite/generate-format/tests/staged/libra.yaml
-
-# Fix import of serde_types
-sed -i '' -e 's/^import serde_types/from .. import serde_types/g' src/pylibra/libra_types/__init__.py src/pylibra/lcs/__init__.py
 
 # Formatting
 . ./venv/bin/activate

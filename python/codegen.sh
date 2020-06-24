17dd90cead3821c3a9e32c14946ea02ba59538d9
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-24 11:08:33 -0700

    [stdlib] transaction builders

--- python/codegen.sh
+++ python/codegen.sh
@@ -2,17 +2,29 @@
 set -euo pipefail
 
 # Generate python code for Libra types and LCS runtime
-cargo install serde-generate --version 0.2.2
+cargo install serde-generate --version 0.3.0
+
+LIBRA=../libra
 
 $HOME/.cargo/bin/serdegen\
     --language python3\
-    --name libra_types\
+    --module-name libra_types\
     --with-runtimes serde lcs\
-    --package-name ..\
+    --serde-package-name ..\
     --target-source-dir src/pylibra\
-    ../libra/testsuite/generate-format/tests/staged/libra.yaml
+    "$LIBRA/testsuite/generate-format/tests/staged/libra.yaml"
+
+# Add transaction builders
+(cd "$LIBRA" && cargo build -p transaction-builder-generator)
+"$LIBRA/target/debug/transaction-builder-generator"\
+    --language python3\
+    --module-name stdlib\
+    --serde-package-name ..\
+    --libra-package-name ..\
+    --target-source-dir src/pylibra\
+    "$LIBRA/language/stdlib/compiled/transaction_scripts/abi"
 
 # Formatting
 . ./venv/bin/activate
 ./venv/bin/pip3 install --upgrade black
-./venv/bin/python3 -m black src/pylibra/*_types/__init__.py src/pylibra/lcs/__init__.py
+./venv/bin/python3 -m black src/pylibra/*_types/__init__.py src/pylibra/lcs/__init__.py src/pylibra/stdlib/__init__.py
--- python/tests/test_lcs.py
+++ python/tests/test_lcs.py
@@ -1,20 +1,20 @@
-# pyre-strict
-
 from pylibra import TransactionUtils
-from pylibra.libra_types import Transaction, ChangeSet, WriteSet, WriteSetMut
+from pylibra import libra_types as libra
+from pylibra import serde_types as st
 from pylibra import lcs
+from pylibra import stdlib
 
 
 def test_lcs_e2e() -> None:
     print("Testing serialization: ")
 
     # pyre-ignore
-    obj = Transaction.WaypointWriteSet(ChangeSet(WriteSet(WriteSetMut(write_set=[])), []))
-    content = lcs.serialize(obj, Transaction)
+    obj = libra.Transaction.WaypointWriteSet(libra.ChangeSet(libra.WriteSet(libra.WriteSetMut(write_set=[])), []))
+    content = lcs.serialize(obj, libra.Transaction)
 
     print("Serialization result: ", content)
 
-    deobj, remaining = lcs.deserialize(content, Transaction)
+    deobj, remaining = lcs.deserialize(content, libra.Transaction)
 
     if remaining:
         raise ValueError("Remaining bytes: ", remaining)
@@ -32,7 +32,7 @@ def test_lcs_e2e_native() -> None:
     print("Testing Deserialization native bytes: ", content)
 
     # pyre-fixme
-    deobj, remaining = lcs.deserialize(content, Transaction.UserTransaction)
+    deobj, remaining = lcs.deserialize(content, libra.Transaction.UserTransaction)
 
     if remaining:
         raise ValueError("Remaining bytes: ", remaining)
@@ -41,8 +41,36 @@ def test_lcs_e2e_native() -> None:
 
     print("Testing serialization again")
 
-    new_content = lcs.serialize(deobj, Transaction.UserTransaction)
+    new_content = lcs.serialize(deobj, libra.Transaction.UserTransaction)
 
     print("Result:", new_content)
 
     assert new_content == content
+
+
+def test_stdlib() -> None:
+    content = TransactionUtils.createSignedP2PTransaction(
+        bytes.fromhex("11" * 32), bytes.fromhex("22" * 16), 255, 1_234_567, expiration_time=123456789,
+    )
+    # pyre-fixme
+    raw_txn = lcs.deserialize(content, libra.Transaction.UserTransaction)[0].value.raw_txn
+
+    # pyre-fixme
+    assert isinstance(raw_txn.payload, libra.TransactionPayload.Script)
+
+    # pyre-fixme
+    token = libra.TypeTag.Struct(
+        value=libra.StructTag(
+            address=libra.AccountAddress(value=(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)),
+            module=libra.Identifier(value="LBR"),
+            name=libra.Identifier(value="LBR"),
+            type_params=[],
+        )
+    )
+    # pyre-fixme
+    payee = libra.AccountAddress((0x22,) * 16)
+    amount = st.uint64(1_234_567)
+    script = stdlib.encode_peer_to_peer_with_metadata_script(token, payee, amount, [], [])
+
+    # We have successfully generated an (unsigned) P2P transaction in python.
+    assert script == raw_txn.payload.value

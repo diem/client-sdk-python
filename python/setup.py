fd158e79f862e047c980cffdc6833e5ad6410b53
Yucong Sun <fallentree@fb.com>
2020-07-01 14:07:43 -0700

    version reset

--- python/setup.py
+++ python/setup.py
@@ -64,7 +64,7 @@ exts = [
 setup(
     name="ivtjfchcukjgtekjrnbllkfrdkvdhdkh",
     # change to 0.1.YYYYMMDDNN on release
-    version="0.4.2020070101",
+    version="0.4.master",
     description="",
     python_requires=">=3.7",
     packages=find_packages("src"),

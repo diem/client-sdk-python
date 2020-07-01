39854cbdbfe28b9139d7988f2724961ff2223993
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-30 18:42:25 -0700

    simplify codegen

--- python/codegen.sh
+++ python/codegen.sh
@@ -4,24 +4,14 @@
 
 set -euo pipefail
 
-# Generate python code for Libra types and LCS runtime
-cargo install serde-generate --version 0.4.0
-
 LIBRA=../libra
-
-$HOME/.cargo/bin/serdegen\
-    --language python3\
-    --module-name libra_types\
-    --with-runtimes serde lcs\
-    --serde-package-name pylibra\
-    --target-source-dir src/pylibra\
-    "$LIBRA/testsuite/generate-format/tests/staged/libra.yaml"
 
 # Add transaction builders
 (cd "$LIBRA" && cargo build -p transaction-builder-generator)
 "$LIBRA/target/debug/transaction-builder-generator"\
     --language python3\
     --module-name stdlib\
+    --with-libra-types "$LIBRA/testsuite/generate-format/tests/staged/libra.yaml"\
     --serde-package-name pylibra\
     --libra-package-name pylibra\
     --target-source-dir src/pylibra\

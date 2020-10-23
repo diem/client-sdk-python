939df7ceb4026b753c89dc5a999dbf37286c0f69
Xiao Li <ilx@fb.com>
2020-10-22 18:26:57 -0700

    regen libra_typesand stdlib code, use new AccountAddress util functions

--- src/libra/libra_types/__init__.py
+++ src/libra/libra_types/__init__.py
@@ -52,6 +52,28 @@ class AccountAddress:
             raise st.DeserializationError("Some input bytes were not read")
         return v
 
+    LENGTH = 16  # type: int
+
+    def to_bytes(self) -> bytes:
+        """Convert account address to bytes."""
+        return bytes(typing.cast(typing.Iterable[int], self.value))
+
+    @staticmethod
+    def from_bytes(addr: bytes) -> "AccountAddress":
+        """Create an account address from bytes."""
+        if len(addr) != AccountAddress.LENGTH:
+            raise ValueError("Incorrect length for an account address")
+        return AccountAddress(value=tuple(st.uint8(x) for x in addr))  # pyre-ignore
+
+    def to_hex(self) -> str:
+        """Convert account address to an hexadecimal string."""
+        return self.to_bytes().hex()
+
+    @staticmethod
+    def from_hex(addr: str) -> "AccountAddress":
+        """Create an account address from an hexadecimal string."""
+        return AccountAddress.from_bytes(bytes.fromhex(addr))
+
 
 @dataclass(frozen=True)
 class BlockMetadata:

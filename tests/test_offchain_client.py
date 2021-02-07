276a5c969106730cd3f824778811b0e2b1900b8e
Xiao Li <ilx@fb.com>
2021-02-07 10:02:32 -0800

    improve LocalAccount and testnet utils
    1. export to dict and import from dict
    2. moved some testnet utils functions into LocalAccount as generic utils:
      1. create transaction from a script
      2. submit and wait for transaction executed
      3. config transaction fields: gas currency code, max gas amount, gas
    unit price and expire duration.
    3. merged testnet gen_vasp_account and gen_account

--- tests/test_offchain_client.py
+++ tests/test_offchain_client.py
@@ -7,8 +7,8 @@ from diem import offchain, testnet
 def test_send_and_deserialize_request(factory):
     client = testnet.create_client()
     receiver_port = offchain.http_server.get_available_port()
-    sender = testnet.gen_vasp_account(client, "http://localhost:8888")
-    receiver = testnet.gen_vasp_account(client, f"http://localhost:{receiver_port}")
+    sender = testnet.gen_account(client, base_url="http://localhost:8888")
+    receiver = testnet.gen_account(client, base_url=f"http://localhost:{receiver_port}")
     sender_client = factory.create_offchain_client(sender, client)
     receiver_client = factory.create_offchain_client(receiver, client)
 

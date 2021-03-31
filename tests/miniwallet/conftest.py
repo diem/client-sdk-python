87d529467d714d254e4f2f9bfc8b9253a7d296d8
Xiao Li <ilx@fb.com>
2021-03-31 14:24:09 -0700

    clean up test_miniwallet_api test

--- tests/miniwallet/conftest.py
+++ tests/miniwallet/conftest.py
@@ -9,12 +9,36 @@ import pytest
 
 @pytest.fixture(scope="package")
 def target_client(diem_client: jsonrpc.Client) -> RestClient:
-    conf = AppConfig(name="target-wallet")
-    print("launch target app with config %s" % conf)
-    conf.start(diem_client)
-    return conf.create_client()
+    return start_app(diem_client, "target-wallet").create_client()
+
+
+@pytest.fixture(scope="package")
+def stub_client(diem_client: jsonrpc.Client) -> RestClient:
+    return start_app(diem_client, "stub-wallet").create_client()
 
 
 @pytest.fixture(scope="package")
 def diem_client() -> jsonrpc.Client:
     return testnet.create_client()
+
+
+@pytest.fixture
+def hrp() -> str:
+    return testnet.HRP
+
+
+@pytest.fixture
+def currency() -> str:
+    return testnet.TEST_CURRENCY_CODE
+
+
+@pytest.fixture
+def travel_rule_threshold(diem_client: jsonrpc.Client) -> int:
+    return diem_client.get_metadata().dual_attestation_limit
+
+
+def start_app(diem_client: jsonrpc.Client, app_name: str) -> AppConfig:
+    conf = AppConfig(name=app_name, enable_debug_api=True)
+    print("launch %s with config %s" % (app_name, conf))
+    conf.start(diem_client)
+    return conf

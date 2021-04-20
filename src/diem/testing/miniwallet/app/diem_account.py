9ae6e5f437da1aeff65a8d53c0540e8a2d2c8650
Xiao Li <ilx@fb.com>
2021-04-20 10:45:42 -0700

    mini-wallet app: init child vasp accounts for sending and receiving payments

--- src/diem/testing/miniwallet/app/diem_account.py
+++ src/diem/testing/miniwallet/app/diem_account.py
@@ -2,7 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from dataclasses import dataclass
-from typing import Tuple, Optional, Union
+from random import randrange
+from typing import Tuple, Optional, Union, List
 from .models import Transaction, RefundReason
 from ... import LocalAccount
 from .... import jsonrpc, identifier, offchain, stdlib, utils, txnmetadata, diem_types
@@ -11,6 +12,7 @@ from .... import jsonrpc, identifier, offchain, stdlib, utils, txnmetadata, diem
 @dataclass
 class DiemAccount:
     _account: LocalAccount
+    _child_accounts: List[LocalAccount]
     _client: jsonrpc.Client
 
     @property
@@ -21,7 +23,7 @@ class DiemAccount:
         return self._account.compliance_key.sign(msg)
 
     def account_identifier(self, subaddress: Union[str, bytes, None] = None) -> str:
-        return self._account.account_identifier(subaddress)
+        return self._get_payment_account().account_identifier(subaddress)
 
     def decode_account_identifier(self, encoded_id: str) -> Tuple[diem_types.AccountAddress, Optional[bytes]]:
         return identifier.decode_account(encoded_id, self.hrp)
@@ -37,8 +39,13 @@ class DiemAccount:
         metadata = cmd.travel_rule_metadata(self.hrp)
         return (metadata, bytes.fromhex(str(cmd.payment.recipient_signature)))
 
-    def submit_p2p(self, txn: Transaction, metadata: Tuple[bytes, bytes]) -> str:
-        to_account, to_subaddress = identifier.decode_account(str(txn.payee), self.hrp)
+    def submit_p2p(
+        self,
+        txn: Transaction,
+        metadata: Tuple[bytes, bytes],
+        by_address: Optional[diem_types.AccountAddress] = None,
+    ) -> str:
+        to_account = identifier.decode_account_address(str(txn.payee), self.hrp)
         script = stdlib.encode_peer_to_peer_with_metadata_script(
             currency=utils.currency_code(txn.currency),
             amount=txn.amount,
@@ -46,4 +53,17 @@ class DiemAccount:
             metadata=metadata[0],
             metadata_signature=metadata[1],
         )
-        return self._account.submit_txn(self._client, script).bcs_serialize().hex()
+        return self._get_payment_account(by_address).submit_txn(self._client, script).bcs_serialize().hex()
+
+    def _get_payment_account(self, address: Optional[diem_types.AccountAddress] = None) -> LocalAccount:
+        if address is None:
+            if self._child_accounts:
+                return self._child_accounts[randrange(len(self._child_accounts))]
+            return self._account
+        for account in self._child_accounts:
+            if account.account_address == address:
+                return account
+        raise ValueError(
+            "could not find account by address: %s in child accounts: %s"
+            % (address.to_hex(), list(map(lambda a: a.to_dict(), self._child_accounts)))
+        )
--- src/diem/testing/miniwallet/config.py
+++ src/diem/testing/miniwallet/config.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from dataclasses import dataclass, field, asdict
-from typing import Dict, Any
+from typing import Dict, Any, List
 from .client import RestClient
 from .app import App, falcon_api
 from .. import LocalAccount
@@ -26,9 +26,11 @@ class AppConfig:
     name: str = field(default="mini-wallet")
     disable_events_api: bool = field(default=False)
     account_config: Dict[str, Any] = field(default_factory=lambda: LocalAccount().to_dict())
+    child_account_configs: List[Dict[str, Any]] = field(default_factory=list)
     server_conf: ServerConfig = field(default_factory=ServerConfig)
-    initial_amount: int = field(default=1_000_000_000_000)
+    initial_amount: int = field(default=3_000_000_000_000)
     initial_currency: str = field(default=testnet.TEST_CURRENCY_CODE)
+    child_account_size: int = field(default=2)
 
     @property
     def logger(self) -> logging.Logger:
@@ -39,6 +41,10 @@ class AppConfig:
         return LocalAccount.from_dict(self.account_config)
 
     @property
+    def child_accounts(self) -> List[LocalAccount]:
+        return list(map(LocalAccount.from_dict, self.child_account_configs))
+
+    @property
     def server_url(self) -> str:
         return self.server_conf.base_url
 
@@ -47,33 +53,26 @@ class AppConfig:
         return RestClient(server_url=self.server_url, name="%s-client" % self.name).with_retry()
 
     def setup_account(self, client: jsonrpc.Client) -> None:
-        acc = client.get_account(self.account.account_address)
-        if not acc or self.need_funds(acc):
-            self.logger.info("faucet mint %s", self.account.account_address.to_hex())
+        if not client.get_account(self.account.account_address):
+            self.logger.info("faucet: mint %s", self.account.account_address.to_hex())
             faucet = testnet.Faucet(client)
             faucet.mint(self.account.auth_key.hex(), self.initial_amount, self.initial_currency)
-        if not acc or self.need_rotate(acc):
+
             self.logger.info("rotate dual attestation info for %s", self.account.account_address.to_hex())
             self.logger.info("set base url to: %s", self.server_url)
             self.account.rotate_dual_attestation_info(client, self.server_url)
 
-    def need_funds(self, account: jsonrpc.Account) -> bool:
-        for balance in account.balances:
-            if balance.currency == self.initial_currency and balance.amount > self.initial_amount / 2:
-                return False
-        return True
-
-    def need_rotate(self, account: jsonrpc.Account) -> bool:
-        if account.role.base_url != self.server_url:
-            return True
-        if not account.role.compliance_key:
-            return True
-        if bytes.fromhex(account.role.compliance_key) != self.account.compliance_public_key_bytes:
-            return True
-        return False
+            self.logger.info("generate child VASP accounts: %s", self.child_account_size)
+            child_account_initial_amount = int(self.initial_amount / (self.child_account_size + 1))
+            for i in range(self.child_account_size):
+                child = self.account.gen_child_vasp(client, child_account_initial_amount, self.initial_currency)
+                self.logger.info("generate child VASP account(%s): %s", i, child.to_dict())
+                self.child_account_configs.append(child.to_dict())
 
     def serve(self, client: jsonrpc.Client) -> threading.Thread:
-        api: falcon.API = falcon_api(App(self.account, client, self.name, self.logger), self.disable_events_api)
+        api: falcon.API = falcon_api(
+            App(self.account, self.child_accounts, client, self.name, self.logger), self.disable_events_api
+        )
 
         def serve() -> None:
             self.logger.info("serving on %s:%s at %s", self.server_conf.host, self.server_conf.port, self.server_url)

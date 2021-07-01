a126ff0c9e88973f598997724333ad70ec1e0dfe
Xiao Li <ilx@fb.com>
2021-07-01 10:41:24 -0700

    rename param typ to klass for consistent naming

--- src/diem/testing/miniwallet/app/json_input.py
+++ src/diem/testing/miniwallet/app/json_input.py
@@ -13,18 +13,18 @@ Validator = Callable[[str, T], Awaitable[None]]
 class JsonInput:
     data: Dict[str, Any]
 
-    async def get_nullable(self, name: str, typ: Type[T], validator: Optional[Validator] = None) -> Optional[T]:
+    async def get_nullable(self, name: str, klass: Type[T], validator: Optional[Validator] = None) -> Optional[T]:
         val = self.data.get(name, None)
         if val is None:
             return None
-        if isinstance(val, typ):
+        if isinstance(val, klass):
             if validator:
                 await validator(name, val)
             return val
-        raise ValueError("%r type must be %r, but got %r" % (name, typ.__name__, type(val).__name__))
+        raise ValueError("%r type must be %r, but got %r" % (name, klass.__name__, type(val).__name__))
 
-    async def get(self, name: str, typ: Type[T], validator: Optional[Validator] = None) -> T:
-        val = await self.get_nullable(name, typ, validator)
+    async def get(self, name: str, klass: Type[T], validator: Optional[Validator] = None) -> T:
+        val = await self.get_nullable(name, klass, validator)
         if val is None:
             raise ValueError("%r is required" % name)
         return val
